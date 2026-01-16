0ddbc448fe0e3998859841092858fb682c63c824
Robin Huang <yuxinlumiere83157@gmail.com>
2026-01-17 02:45:56 +1300

    Update CV

---
+++ src/data/certificates.ts
@@ -0,0 +1,26 @@
+export const certificates = [
+  {
+    name: 'Foundational C# with Microsoft',
+    issuer: 'freeCodeCamp',
+    date: '',
+    credentialUrl: '',
+    pdfPath: '',
+    highlights: [],
+  },
+  {
+    name: 'Google Cloud Fundamentals: Core Infrastructure',
+    issuer: 'Google',
+    date: '',
+    credentialUrl: '',
+    pdfPath: '',
+    highlights: [],
+  },
+  {
+    name: 'AWS Cloud Practitioner Essentials',
+    issuer: 'Amazon',
+    date: '',
+    credentialUrl: '',
+    pdfPath: '',
+    highlights: [],
+  },
+]
